07b3ef90fed4819c0e3c050f034fee2ce50144c8
Marie Yao <mossia.y@gmail.com>
2023-02-24 18:11:43 +0100

    les exos faits depuis le dernier commit

--- maths.py
+++ maths.py
@@ -1,3 +1,3 @@
-# Complete the findPoint function in the editor below.
-# findPoint has the following parameters:
-# int px, py, qx, qy: x and y coordinates for points p and q
+#A person is getting ready to leave and needs a pair of matching socks. If there are n colors of socks in the drawer, how many socks need to be removed to be certain of having a matching pair?
+#Example n=2
+# There are  2 colors of socks in the drawer. If they remove  2 socks, they may not match. The minimum number to insure success is 3.#
--- ql.sql
+++ ql.sql
@@ -48,4 +48,17 @@ ORDER BY LENGTH(CITY) DESC, CITY ASC
 LIMIT 1;
 
 --Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.--
-SELECT DISTINCT CITY FROM STATION WHERE substr(CITY,1,1) IN ('A','E','I','O','U');
+SELECT DISTINCT CITY FROM STATION WHERE (CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u');
+
+-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.-- 
+SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY,1) IN('A','E','I','O','U') AND RIGHT(CITY,1) IN('a','e','i','o','u');
+
+-- Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.--
+SELECT DISTINCT CITY FROM STATION WHERE NOT LEFT(CITY,1) IN('A','E','I','O','U') ;
+ --  Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.--
+ SELECT DISTINCT CITY FROM STATION WHERE NOT RIGHT(CITY,1) IN('a','e','i','o','u');
+ -- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.--
+ SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY,1) NOT IN('A','E','I','O','U') OR RIGHT(CITY,1) NOT IN('a','e','i','o','u');
+ -- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.--
+ SELECT DISTINCT CITY FROM STATION WHERE LEFT(CITY,1) NOT IN('A','E','I','O','U') AND RIGHT(CITY,1) NOT IN('a','e','i','o','u');
+ -- --
